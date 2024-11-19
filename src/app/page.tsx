03636a5b5e49b344ea7953d27f424ca46381807d
soulfulDreamer <kazishahhamza@outlook.com>
2024-11-19 18:39:48 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
+import FeaturedProperties from "@/components/FeaturedProperties";
+import Services from "@/components/Services";
+import AboutUs from "@/components/AboutUs";
+import Testimonials from "@/components/Testimonials";
+import Faq from "@/components/Faq";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HowItWorks />
+      <FeaturedProperties />
+      <Services />
+      <AboutUs />
+      <Testimonials />
+      <Faq />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
